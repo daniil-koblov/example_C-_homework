89873751d80fb71655de22c26274d847726705e2
Daniil Koblov <1koblov.danil1@gmail.com>
2022-08-26 01:53:43 +0300

    true add TheSecondTask program

---
+++ FirstHomeWork/TheSecondTask/Program.cs
@@ -0,0 +1,30 @@
+﻿// Вторая задача на нахождение максимального из трех чисел.
+Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    if(num1 > num3)
+    {
+        Console.WriteLine(num1);
+    }
+    else
+    {
+        Console.WriteLine(num3);
+    }
+}
+else
+{
+    if(num2 > num3)
+    {
+        Console.WriteLine(num2);
+    }
+    else
+    {
+        Console.WriteLine(num3);
+    }
+}
