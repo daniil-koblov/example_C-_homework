6eb4bfe322ace81d341d8c6b918113e8386510c7
Daniil Koblov <1koblov.danil1@gmail.com>
2022-08-26 02:37:27 +0300

    add TheFourthTask program

---
+++ FirstHomeWork/TheFourthTask/Program.cs
@@ -0,0 +1,21 @@
+﻿// Четвертая задача на вывод четных чисел в ряде от 1 до N.
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = 1;
+int count = 1;
+
+while(count <= num)
+{
+    if(num1 % 2 == 0)
+    {
+        Console.WriteLine(count);
+        Console.WriteLine(" ");
+        count = count + 1;
+        num1 = num1 + 1;
+    }
+    else
+    {
+        count = count + 1;
+        num1 = num1 + 1;
+    }
+}
