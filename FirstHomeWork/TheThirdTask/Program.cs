6aa64556554cc71c59789a65a72beb391ff2e5b5
Daniil Koblov <1koblov.danil1@gmail.com>
2022-08-26 02:03:28 +0300

    add TheThirdTask program

---
+++ FirstHomeWork/TheThirdTask/Program.cs
@@ -0,0 +1,12 @@
+﻿// Третья задача на проверку четности числа.
+Console.WriteLine("Напишите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}
