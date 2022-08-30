d35a1d8eb4ed54a7b0e4ca2d2ec9cfbbba42898c
Daniil Koblov <1koblov.danil1@gmail.com>
2022-08-30 04:07:55 +0300

    add task2_2

---
+++ SecondHomeWork/task2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вторая задача на вывод третьей цифры, введенного числа.
+
+
+Console.WriteLine("Введите число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(num1);
+if (str.Length > 2)
+{
+    Console.WriteLine(str[2]);
+}
+else
+{
+    Console.WriteLine("В введенном числе нет третьей цифры");
+}
