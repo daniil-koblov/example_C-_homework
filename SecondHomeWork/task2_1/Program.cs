ed062ec3b0561247417483f88bd1790f202c4f0a
Daniil Koblov <1koblov.danil1@gmail.com>
2022-08-30 03:01:49 +0300

    add task2_1

---
+++ SecondHomeWork/task2_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Первая задача на вывод второй цифры, введенного трехзначного числа.
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = num1 / 10;
+int num3 = num2 % 10;
+
+Console.WriteLine($"{num3}");
