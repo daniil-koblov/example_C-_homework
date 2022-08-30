ec31aebb80e56936a023fbe2bc27c29fd18985f9
Daniil Koblov <1koblov.danil1@gmail.com>
2022-08-30 03:05:25 +0300

    add task2_1_1

---
+++ SecondHomeWork/task2_1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Первая задача на вывод второй цифры, введенного трехзначного числа.
+// Второй вариант, в котором случайное число.
+Console.Clear();
+
+int num1 = new Random().Next(100, 1000);
+Console.WriteLine(num1);
+int num2 = num1 / 10;
+int num3 = num2 % 10;
+
+Console.WriteLine($"{num3}");
