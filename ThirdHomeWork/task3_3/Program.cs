4151e6ddb68e9b573c7bc704828e83148227c338
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-02 05:09:19 +0300

    add task3_3

--- ThirdHomeWork/task3_3/Program.cs
+++ ThirdHomeWork/task3_3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Третья задача. Программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while(count <= num)
+{
+    Console.Write($"{count*count*count} ");
+    count++;
+}
+Console.WriteLine();
