49b1d8e70508f07c5c25c282c0867eb7f44f2571
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-02 18:42:32 +0300

    add 2 task3_1

--- ThirdHomeWork/task3_1/Program.cs
+++ ThirdHomeWork/task3_1/Program.cs
@@ -5,9 +5,25 @@ Console.Write("Введите пятизначное число:");
 string num = Console.ReadLine();
 if ((num[0] == num[4]) && (num[1] == num[3]))
 {
-    Console.WriteLine("Да");
+    Console.WriteLine("Является палиндроммом");
 }
 else
 {
-    Console.WriteLine("Нет");
+    Console.WriteLine("Не является палиндроммом");
+}
+
+/* Альтернативное решение */
+Console.Write("Введите пятизначное число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num2 >= 10000 && num2 <= 99999)
+{
+    if((num2 / 10000 == num2 % 10) && (num2 % 10000 / 1000 == num2 % 100 / 10))
+    {
+        Console.WriteLine("Является палиндроммом");
+    }
+    else
+    {
+        Console.WriteLine("Не является палиндроммом");
+    }
 }
