5d9a4ec6acd4cf264f521b82bacca28b0d14d362
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-05 19:23:19 +0300

    add task4_2

--- FourthHomeWork/task4_2/Program.cs
+++ FourthHomeWork/task4_2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вторая задача. Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+int SumOfNum(int a)
+{
+    int sum = 0;
+    if(a > 0)
+    {
+        while (a > 0)
+        {
+            sum = sum + a % 10;
+            a = a / 10;
+        }
+        return sum;
+    }
+    else if(a == 0)
+    {
+        return sum;
+    }
+    else
+    {
+        int b = 1;
+        b = a * 2;
+        a = a - b;
+        while (a > 0)
+        {
+            sum = sum + a % 10;
+            a = a / 10;
+        }
+        return sum;
+    }
+}
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр введенного числа {num1} равна {SumOfNum(num1)}");
--- FourthHomeWork/task4_3/Program.cs
+++ FourthHomeWork/task4_3/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+b = a * 2;
+a = a - b;
+Console.WriteLine(a);
