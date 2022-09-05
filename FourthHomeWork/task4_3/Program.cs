a94d25c147410b15428595ce43f24bd27f216f19
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-05 19:32:21 +0300

    add task4_3

--- FourthHomeWork/task4_3/Program.cs
+++ FourthHomeWork/task4_3/Program.cs
@@ -1,8 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// Третья задача. Программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
 
-int a = Convert.ToInt32(Console.ReadLine());
-int b = 1;
-b = a * 2;
-a = a - b;
-Console.WriteLine(a);
+void FillArray(int[] a)
+{
+    int length = a.Length;
+    if(length > 0)
+    {
+        for(int i = 0; i < a.Length; i++)
+            {
+                int random = new Random().Next(-2147483648, 2147483647); 
+                a[i] = random;
+                Console.Write($"{a[i]} ");
+            }
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine("Длинна должна быть равна 1 или больше.");
+    }
+}
+
+Console.WriteLine("Введите длинну массива");
+int len = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [len];
+FillArray(array);
