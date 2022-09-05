cc668f59df73d8422e6fc74a6a8176c8d82bc76e
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-05 17:30:30 +0300

    add task4_1

---
+++ FourthHomeWork/task4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+void Degree(int a, int b)
+{
+    int res = 1;
+    for(int count = 0; count < b; count++)
+    {
+        res = res * a;
+    }
+    Console.WriteLine(res);
+}
+
+Console.WriteLine("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Degree(A, B);
