ab1224b3a44dbd1ae99b9c7190d9a43f0f4a63ae
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-08 17:22:45 +0300

    add task5_1

--- FifthHomeWork/task5_1/Program.cs
+++ FifthHomeWork/task5_1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Первая задача. Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.Clear();
+
+void FillArray(int[] a)
+{
+    for(int i = 0; i < a.Length; i++)
+    {
+        int random = new Random().Next(100, 1000);
+        a[i] = random;
+        Console.Write($"{a[i]} ");
+    }
+    Console.WriteLine();
+    int count = 0;
+    for(int i = 0; i < a.Length; i++)
+    {
+        if(a[i] % 2 == 0) count++;
+        else count+=0;
+    }
+    Console.WriteLine($"Кол-во четных чисел массива равно {count}");
+}
+
+Console.WriteLine("Введите длинну массива");
+int len = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [len];
+FillArray(array);
