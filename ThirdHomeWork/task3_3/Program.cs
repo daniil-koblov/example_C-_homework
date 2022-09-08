688e2f9828121de6fecdbb6fbbf4e9ad5f1172c8
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-08 17:13:04 +0300

    second add task3_3

--- ThirdHomeWork/task3_3/Program.cs
+++ ThirdHomeWork/task3_3/Program.cs
@@ -1,12 +1,18 @@
 ﻿// Третья задача. Программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 Console.Clear();
+
+void Cubes(int a)
+{
+    int count = 1;
+
+    while(count <= a)
+    {
+        Console.Write($"{count*count*count} ");
+        count++;
+    }
+    Console.WriteLine();
+}
+
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
-int count = 1;
-
-while(count <= num)
-{
-    Console.Write($"{count*count*count} ");
-    count++;
-}
-Console.WriteLine();
+Cubes(num);
